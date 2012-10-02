6caa599bccefe2c21df83addbf2ef7df5c79c1f6
Udit <udit@manektala.com>
2012-10-01 21:28:41 -0700

    refactoring

--- lib/dutchess.rb
+++ lib/dutchess.rb
@@ -2,9 +2,11 @@ require "dutchess/version"
 
 module Dutchess
 
-  def self.say
-    dutchism = YAML.load_file(File.join(File.dirname(__FILE__),"yaml/dutchisms.yml")).sample
-    puts dutchism
-    dutchism
+  def self.say_something
+    puts YAML.load_file(File.join(File.dirname(__FILE__),"dutchess/yaml/dutchisms.yml")).sample
+  end
+
+  def self.get_random_dutchism
+    YAML.load_file(File.join(File.dirname(__FILE__),"dutchess/yaml/dutchisms.yml")).sample
   end
 end
