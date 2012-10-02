4ad0d3eb680ac42ea7ebf1e132e82df4df5920f8
Udit <udit@manektala.com>
2012-10-01 21:34:05 -0700

    stuff

--- lib/dutchess.rb
+++ lib/dutchess.rb
@@ -3,7 +3,9 @@ require "dutchess/version"
 module Dutchess
 
   def self.say_something
+    puts "\n*"
     puts YAML.load_file(File.join(File.dirname(__FILE__),"dutchess/yaml/dutchisms.yml")).sample
+    puts "*\n"
   end
 
   def self.get_random_dutchism
